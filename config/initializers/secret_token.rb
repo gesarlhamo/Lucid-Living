155223654f53d9b69ba901c6caea1970e83c68c4
gesarlhamo80@gmail.com <gesarlhamo80@gmail.com>
2012-12-13 15:53:46 -0600

    ran rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LucidLiving::Application.config.secret_token = '7ccca8219fafd8dd14254e55144490b861272812a87495e83e6e060ed6343fe21e7b829cdfd67e2862a0a54380a13be733c20def4d2d6c428849fd12c29409fa'
